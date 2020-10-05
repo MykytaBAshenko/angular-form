9f4578ebc7f4f44b08f55935e48699f23c539146
Mykyta Bashenko <45737720+MykytaBAshenko@users.noreply.github.com>
2020-10-05 18:04:40 +0300

    dasad

--- AngularForm/src/app/form/form.component.ts
+++ AngularForm/src/app/form/form.component.ts
@@ -9,8 +9,14 @@ import { MatSnackBar,MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/materia
 export class FormComponent implements OnInit {
   sendForm: FormGroup;
   whichHappen: Number;
+  isFirstEmailOk;
+  isSecondEmailOk;
+  emailStringValidator = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
   constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
     this.createForm();
+    this.isFirstEmailOk = 1;
+    this.isSecondEmailOk = 1;
+
    }
 
   ngOnInit(): void {
@@ -20,10 +26,24 @@ export class FormComponent implements OnInit {
     this.sendForm = this.fb.group({
       name1: ['', [ Validators.required] ],
       name2: ['', [ Validators.required] ],
-      email1: ['', [Validators.email, Validators.required] ],
+      email1: ['', [Validators.email, Validators.required ] ],
       email2: ['', [Validators.email, Validators.required] ]
-
     });
+  }
+  checkFirstEmail(v) {
+    if(this.emailStringValidator.test(v)){
+      this.isFirstEmailOk = 1;
+    }
+    else 
+    this.isFirstEmailOk = 0;
+    console.log(this.isFirstEmailOk)
+  }
+  checkSecondEmail(v) {
+    if(this.emailStringValidator.test(v)){
+      this.isSecondEmailOk = 1;
+    }
+    else 
+    this.isSecondEmailOk = 0;
   }
   formError(){
     this.whichHappen = -1;
